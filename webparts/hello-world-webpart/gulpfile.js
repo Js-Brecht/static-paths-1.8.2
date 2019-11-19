53241a09c65b28bad8e5303872b338a56a04e30d
Jeremy Albright <myrlin1@manaollc.com>
2019-11-19 08:40:56 -1000

    patch implementation

--- webparts/hello-world-webpart/gulpfile.js
+++ webparts/hello-world-webpart/gulpfile.js
@@ -1,7 +1,24 @@
 'use strict';
 
+const path = require('path');
 const gulp = require('gulp');
 const build = require('@microsoft/sp-build-web');
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
+
+const config = build.getConfig();
+config.staticPaths = [
+  {
+    path: path.resolve("../../common/temp/node_modules"),
+    url: "/common/temp/node_modules"
+  },
+  // {
+  //   path: path.resolve("../../common/temp/node_modules"),
+  //   url: "/../../common/temp/node_modules"
+  // },
+  {
+    path: path.resolve("../../custom"),
+    url: "/custom"
+  }
+]
 
 build.initialize(gulp);
